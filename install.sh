252bf2f2e7d0c4f466c74c6b4e831de6ff80d5db
ZetZhang <13660591402@163.com>
2020-04-11 10:30:28 +0800

    cut code

--- install.sh
+++ install.sh
@@ -6,16 +6,20 @@ export USER=`whoami`
 if [ ! -d ~/.hci ]; then
     echo -e "configure manjaro?(y|yes)"
     read -t 7 -u 0 choice
-    if [[ "{y, yes}" =~ $choice ]]; then mkdir -p ~/.hci && touch ~/.hci/.c1 || exit 127
-    else exit 0
-    fi
+    if [[ "{y, yes}" =~ $choice ]]; then mkdir -p ~/.hci && touch ~/.hci/.c1
+    else exit 0; fi
+    
+    echo -e "simple installtion(s|simple) or full installtion(f|full)"
+    read -t 7 -u 0 way
+    if [[ "{s, simple}" =~ $way ]]; then touch ~/.hci/.simple
+    elif [[ "{f, full}" =~ $way ]]; then touch ~/.hci/.full
+    else exit 0; fi
 fi
 jmpback=`pwd`
 
 # source server init
 declare exi=`grep '\[archlinuxcn\]' /etc/pacman.conf | wc -l`
 if test $exi -eq 0; then
-    #trap "wait..." SIGINT
     cd $jmpback
     #sudo pacman-mirrors --fasttrack
     su - -c "echo -E \"[archlinuxcn]
@@ -23,7 +27,6 @@ SigLevel = Optional TrustedOnly
 Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch\" >> /etc/pacman.conf"
     sudo sed -i "s/^# \(Color\)/\1/" /etc/pacman.conf
     sudo pacman -Syy && sudo pacman -S archlinuxcn-keyring
-    #trap -- SIGINT
 fi
 
 # install
@@ -31,23 +34,17 @@ ma='sudo pacman -S --noconfirm --needed'
 ya='sudo pacman -S --noconfirm --needed'
 $ma base-devel yay
 $ya -Syu
-$ma curl
-$ma wget
-$ma google-chrome
+$ma {curl,wget}
 
 # install tmux
-$ma tmux
-$ma xclip
+$ma {tmux,xclip}
 if [ -f ~/.hci/.c1 ]; then
-    #trap "wait..." SIGINT
     # Tpm
     mkdir -p ~/.tmux/plugins
     cp ./tmux_conf ~/.tmux.conf
     cd ~/.tmux/plugins && git clone https://github.com/tmux-plugins/tpm
-    #trap -- SIGINT
     mv ~/.hci/.c1 ~/.hci/.c2
 fi
-#[ $? -eq 0 ] && rm -rf ~/.hci/.c1 || exit 127
 tmux new -d -s test && tmux source ~/.tmux.conf && tmux kill-session -t test
 # you should enter session and execute command: `prefix r` & `prefix I`
 # tmux Resurrect
@@ -56,8 +53,15 @@ tmux new -d -s test && tmux source ~/.tmux.conf && tmux kill-session -t test
 #tmux run-shell ~/.tmux/plugins/tmux-resurrect/resurrect.tmux
 
 $ma zsh
-#trap "wait.." SIGINT
 if [ -f ~/.hci/.c2 ]; then
+    cd $jmpback && mv ~/.hci/.c2 ~/.hci/.c3
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# install zsh && ohmyzsh
+if [ -f ~/.hci/.c3 ]; then
+    cd $jmpback
+    $ma {fzf,autojump,the_silver_searcher,thefuck}
     sed -i "/^plugins=(/a\ \tgit\n\tzsh-syntax-highlighting\n\tautojump\n\tfzf\n\tzsh-autosuggestions\n\tweb-search\n\textract\n)" ~/.zshrc
     sed -i "s/\(^plugins=(\)git)/\1/" ~/.zshrc
     sed -i "s/^# \(export LANG\)/\1/" ~/.zshrc
@@ -72,75 +76,72 @@ if [ -f ~/.hci/.c2 ]; then
             #cat {}) 2> /dev/null | head -500'" >> ~/.zshrc
             echo -E "# fzf
             export FZF_DEFAULT_COMMAND='rg --files --hidden'" >> ~/.zshrc
-
-    #source ~/.zshrc
-    mv ~/.hci/.c2 ~/.hci/.c3
-fi
-#trap SIGINT
-
-# install zsh && ohmyzsh
-if [ -f ~/.hci/.c3 ]; then
-    cd $jmpback
+    echo "eval $(thefuck --alias)" >> ~/.zshrc
+    source ~/.zshrc
     #sudo chsh -s /bin/zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    $ma fzf
-    $ma autojump
-    $ma the_silver_searcher
     mv ~/.hci/.c3 ~/.hci/.c4
 fi
-#[ $? -eq 0 ] && rm -rf ~/.hci/.c2 || exit 127
 
 # zsh-autosuggestions
-[ -f ~/.hci/.c4 ] && git clone
-https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && mv ~/.hci/.c4 ~/.hci/.c5
-#[ $? -eq 0 ] && rm -rf ~/.hci/.c3 || exit 127
-
+[ -f ~/.hci/.c4 ] && git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && mv ~/.hci/.c4 ~/.hci/.c5
 # zsh-syntax-highlighting
-[ -f ~/.hci/.c5 ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && rm -rf ~/.c5
-#[ $? -eq 0 ] && rm -rf ~/.hci/.c4 || exit 127
+[ -f ~/.hci/.c5 ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && rm -rf ~//.hci/.c5
 
 $ma rust
 cd ~/.ephemeral_folder
 git clone https://github.com/ZetZhang/vim-congiration-installer.git && source ./vim-congiration-installer/install.sh
 
-$ma jdk8-openjdk
-$ma jdk10-openjdk
-$ma jdk13-openjdk
-archlinux-java status
-archlinux-java set java-8-jdk
+# simple
+if test -f ~/.hci/.simple ; then
+    $ma {\
+        jdk8-openjdk,\
+        jdk10-openjdk,\
+        jdk13-openjdk}
+    archlinux-java status
+    archlinux-java set java-8-jdk
 
-$ma code
-$ma clion
-$ma pycharm-professional
-$ma vmware-workstation
-$ma qtcreator
+    $ma {\
+        bar,\
+        gdb,\
+        crash,\
+        strace,\
+        zeromq,\
+        thefuck,\
+        you-get,\
+        cppcheck,\
+        systemtap,\
+        cppman-git,\ # cppman -c
+        kcachegrind}
+fi
 
+# full
+if test -f ~/.hci/.full ; then
+    $ma {\
+        code,\
+        clion,\
+        pycharm-professional,\
+        vmware-workstation,\
+        qtcreator}
 
-$ma bat
-$ma gdb
-$ma crash
-$ma strace
-$ma zeromq
-$ma thefuck
-$ma you-get
-$ma cppcheck
-$ma systemtap
-$ma cppman-git
-# cppman -c
-$ma kcachegrind
+    $ma {\
+        lrzsz,\
+        nutstore,\
+        plantuml,\
+        cairo-dock,\
+        electronic-wechat,\
+        shadowsocks-qt5,\
+        wps-office,\
+        ttf-wps-fonts,\
+        netease-cloud-music,\
+        typora,\
+        timeshift,\
+        nemiver,\
+        google-chrome,\
+        mpv}
 
-$ma lrzsz
-$ma nutstore
-$ma plantuml
-$ma cairo-dock
-$ma electronic-wechat
-$ma shadowsocks-qt5
-$ma wps-office ttf-wps-fonts
-$ma netease-cloud-music
-$ma typora
-#$ma timeshift
-$ma nemiver
-$ma mpv
-#echo "eval $(thefuck --alias)" >> ~/.zshrc
-$ya -S debtap
-$ya -S xmind
+    $ya {\
+        debtap,\
+        xmind}
+fi
+
+[ $? -eq 0 ] && rm -rf ~/.hci
